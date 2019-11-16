4a474821a5df2583dfce7fa61061a6b68dc670c1
Zheming Gu <zheminggugu@gmail.com>
2019-11-16 03:37:26 -0500

    add scroll up animation
    now hit button can scroll page with animaiton

--- blog_assets/js/main.js
+++ blog_assets/js/main.js
@@ -4,8 +4,7 @@
 jQuery(document).ready(function ($) {
 	"use strict";
 
-// scroll Up
-
+	// ============scroll Up==================
     $(window).scroll(function(){
         if ($(this).scrollTop() > 600) {
             $('.scrollup').fadeIn('slow');
@@ -16,8 +15,25 @@ jQuery(document).ready(function ($) {
     $('.scrollup').click(function(){
         $("html, body").animate({ scrollTop: 0 }, 1000);
         return false;
-    });	
+    });
 
-
-    //End
+	
+	//=============smooth scrolling ============
+	$('.smoothscroll').on('click', function (e) {
+		e.preventDefault();
+		var target = this.hash, $target = $(target);
+		$('html, body').stop().animate({'scrollTop': $target.offset().top},600, 'swing').promise().done(function () {
+			// check if menu is open
+			if ($('body').hasClass('menu-is-open')) 
+			{
+				$('.menu-toggle').trigger('click');
+			}
+			window.location.hash = target;
+		  });
+	});
+	
+	
+	
 });
+
+
